608f4a45e9218d11e60d5a1ee0071bda62d776c6
kwan3217 <kwan3217@gmail.com>
2021-05-29 14:23:19 -0600

    Added import of numpy to test

--- tests/test_picturebox.py
+++ tests/test_picturebox.py
@@ -1,7 +1,9 @@
 import pytest
+import numpy as np
+import matplotlib.pyplot as plt
 from picturebox import PictureBox
 
-def test_PictureBox():
+def test_PictureBox(tmp_path):
     pb=PictureBox(1280,720)
     pb.stroke(np.array([0,200]),np.array([0,90]),color="#ff0000")
     pb.fill(np.array([100,200,200,100]),np.array([100,100,200,200]),color="#ff8000")
@@ -11,9 +13,9 @@ def test_PictureBox():
     rho0=1.2 #density at 0 altitude kg/m**3
     q=vel**2*rho0*np.exp(-alt/h)
     pb.image(200,200,300,300,np.flipud(np.log10(q)))
-    pb.fill(np.array([201,201,299,299]),np.array([201,299,299,201]),color="#ffff00")
+    pb.fill(np.array([225,225,275,275]),np.array([225,275,275,225]),color="#ffff00")
     pb.text(300,300,"Hello, World!")
-    pb.savepng("test_PictureBox.png")
+    pb.savepng(str(tmp_path/"test_PictureBox.png"))
     plt.show()
 
 

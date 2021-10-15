6dfa47abefc2748d1d6c4c03f3d07e42625559a5
Chris Jeppesen <jeppesen@lasp.colorado.edu>
2021-10-14 21:34:25 -0600

    Removed reference to Bezier (functionality now pulled from KwanMath)

--- picturebox/__init__.py
+++ picturebox/__init__.py
@@ -1,4 +1,3 @@
 from .PictureBox import PictureBox
 from .actor import Actor, EnterActor, Axis, TableColumn, TableGrid, Text, Plot, Function, Field, perform, shadowcolor, linterp, smooth, tc
-from .bezier import Bezier
 from .path import Path

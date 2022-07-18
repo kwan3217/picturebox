d31b90c31689da65e19fcc3ee47dcbd0f823a490
kwan3217 <kwan3217@gmail.com>
2022-07-18 16:12:35 -0600

    Added dunders to make PictureBox a context manager

--- picturebox/PictureBox.py
+++ picturebox/PictureBox.py
@@ -22,6 +22,10 @@ class PictureBox():
         self.autodraw=autodraw
         self.resetM(origin=origin)
         plt.pause(0.001)
+    def __enter__(self):
+        return self
+    def __exit__(self,exc_type,exc_value,exc_traceback):
+        plt.close(self.fig)
     @staticmethod
     def Mtransform(M,xdata,ydata):
         rdata = vcomp((xdata, ydata, 1))
